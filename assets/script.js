fcbbb122aa37236d25b57b161780a49f75a77240
noah.hayne@gmail.com <noah.hayne@gmail.com>
2020-04-16 21:48:00 -0400

    basic functionality

--- assets/script.js
+++ assets/script.js
@@ -35,16 +35,17 @@ var APIKey  = "166a433c57516f51dfab1f7edaed8413"
 var cityName="springfield";
 
 
-// // AJAX CALL JUST FOR WEATHER
-// $.ajax({
-//   // URL WITH MY KEY  
-//   url: queryURL,
-//   // METHOD IS JUST "GET" INFORMATION FROM API 
-//   method: "GET",
-
-// }).then(function(response){  
-//   console.log(response);
-// });
+// AJAX CALL JUST FOR WEATHER
+$.ajax({
+    // URL WITH MY KEY  
+    url: "https:api.openweathermap.org/data/2.5/weather?q=SPRINGFIELD&apikey=166a433c57516f51dfab1f7edaed8413",
+    // METHOD IS JUST "GET" INFORMATION FROM API 
+    method: "GET",
+    
+}).then(function(response){  
+    cityName=response.name;
+    
+});
 
 // // AJAX CALL FOR 5 DAY FORECAST
 // function getFiveDayForecast(){
@@ -72,47 +73,97 @@ var storedCities=[];
 
 
 
-
 // SEARCH BUTTON CLICK HANDLER
 $("#searchButton").on("click", function(){
     
     // PREVENTS THE PAGE FROM RELOADING 
     event.preventDefault();
-    // SET SEARCHED CITY TO THE VALUE OF THE FORM INPUT
-    searchedCity=$("#searchedCity").val();
     
-    // MAKE SURE THE PORM IS POPULATED AND THE SEARCHED CITY IS NOT IN STORED CITIES
+    // SET SEARCHED CITY TO THE VALUE OF THE FORM INPUT
+    var searchedCity=$("#searchedCity").val();
+    
+    // MAKE SURE THE FORM IS POPULATED AND THE SEARCHED CITY IS NOT IN STORED CITIES
     if(searchedCity!="" && storedCities.indexOf(searchedCity)===-1){
+        
         // CONSOLE LOGS SEARCHED CITY
         console.log(searchedCity);
+        
         // SETS THE FORM INPUT BACK TO ""
         $("#searchedCity").val("");
+        
         // ADDS THE SEARCHED CITY TO THE STORED CITIES
         storedCities.push(searchedCity);
-        console.log(storedCities);
-        saveToLocal(storedCities);
+        
+        
+        // SAVE TO LOCAL STORAGE
+        saveToLocal();
+        
+        // GET CITIES FROM STORAGE AND DISPLAY 
         getFromLocal();
     }
 })
-// SAVES ARRAY TO LOCAL STORAGE 
-function saveToLocal(storedCities){
+
+// SAVES STORED CITIES AND CURRENT CITY TO LOCAL STORAGE 
+function saveToLocal(){
     localStorage.setItem("storedCities",JSON.stringify(storedCities));
-}
-// GET ARRAY FROM LOCAL STORAGE
-function getFromLocal(){
-    storedString=localStorage.getItem("storedCities");
-    storedCities=JSON.parse(storedString);
-    console.log(storedCities);   
-    renderButtons();
+    
 }
 
+// GET ARRAY AND CURRENT CITY FROM LOCAL STORAGE
+function getFromLocal(){
+    
+    // GET THE STORED STRING FROM LOCAL STORAGE
+    storedString=localStorage.getItem("storedCities");
+    
+    // PARSE THE STRING TO TURN IT BACK INTO AN OBJECT
+    storedCities=JSON.parse(storedString);
+    
+    // CREATE BUTTONS FOR EACH ITEM  
+    renderButtons();
+    
+    
+    
+    
+}
 
 // FUNTION TO RENDER THE BUTTONS FROM THE LOCALLY STORED ARRAY
 function renderButtons(){
+    // CLEAR THE SAVED CITY BUTTONS
     $("#savedCities").empty();
+    // FOR EACH ITEM IN THE STORE CITIES ARRAY
     for (var i = 0; i < storedCities.length; i++) {  
+        // CREATE A NEW BUTTON
         newbutton=$("<button>");
+        // SET THE BUTTON TEXT TO THE CITY NAME
         newbutton.text(storedCities[i]);
+        newbutton.attr("cityName",storedCities[i])
+        // APPEND THE BUTTON TO THE SAVED CITIES BUTTON DIV
         $("#savedCities").append(newbutton);
     }
+}
+
+
+
+// FORCAST SYMBOL
+// forecast.symbol
+// forecast.symbol.number Weather condition id
+// forecast.symbol.name Weather condition
+// forecast.symbol.var Weather icon id
+
+
+
+// CLICK HANDLER FOR THE SAVED CITY BUTTONS
+$("#savedCities").on("click", "button",function(){
+    // SETTING THE CURRENT DISPLAY CITY TO CLICKED TARGET
+    buttonClicked=event.target;
+    console.log(buttonClicked);
+    saveToLocal();
+    getFromLocal();
+    // $("#displayCurrentCity")
+})
+
+// FUNCITON TO UPDATE THE DISPLAY WITH CURRENT CITY
+function updateDisplay(){
+    
+    $("#displayCurrentCity").text("");
 }
